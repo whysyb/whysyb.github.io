5c372b3380002435a7ef0ce8670221fc5279e86d
Thomas Stephan <thomas.stephan2120@gmail.com>
2017-08-25 18:14:46 +0200

    Add new portfolio files

--- js/index.js
+++ js/index.js
@@ -26,32 +26,63 @@ function weeGrid(){
 	}
 }
 
-function projects(){
-	//open URL in new tab
-	// TODO find bug on chrome
-	/*let links = findAll('.project a');
-	if(links.length > 0){
-		for(var i = 0; i < links.length; i++){
-			links[i].setAttribute('onclick', 'window.open(this.href); return false;');
-			links[i].setAttribute('onkeypress', 'window.open(this.href); return false;');
-		}
-	}*/
-}
+function uiux(){
+	// working process
+	let workingbloc = document.querySelectorAll('.working-process-bloc');
+	let maxChar = 52;
 
-//jQuery functions
-// TODO animatedScroll -> Refused to load the script 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js' because it violates the following Content Security Policy directive: "script-src 'self' https://www.googleapis.com".
-/*$(document).ready(function(){
-	function animatedScroll(){
-		$('a[href^=#]').click(function(){
-			var $target = $(this.hash);
-			if ($target.length) {
-				var targetOffset = $target.offset().top;
-				$('html,body').delay(50).animate({scrollTop: targetOffset}, 200);
-				return false;
+	if(workingbloc.length>0){
+		for(let i = 0; i < workingbloc.length; i++){
+
+			let wkbcontent = workingbloc[i].querySelector('p');
+			let wbkheight = wkbcontent.offsetHeight
+			if(wkbcontent.innerText.length > maxChar){
+				wkbcontent.classList.add('larger');
+				wkbcontent.style.height = 50 + 'px';
 			}
-		});
+
+
+			workingbloc[i].addEventListener('click', function(){
+				if(this.classList.contains('open')){
+					this.classList.remove('open')
+					wkbcontent.style.height = 50 + 'px';
+				}else{
+					this.classList.add('open')		
+					wkbcontent.style.height = wbkheight + 'px';	
+				}
+			})
+		}
 	}
-});*/
+	window.addEventListener('scroll', function(){
+		if(this.scrollY >= 250){
+			document.querySelector('body').classList.add('scrolled');
+			document.querySelector('body').classList.add('scrolling');			
+		}else{
+			document.querySelector('body').classList.remove('scrolling');			
+		}
+	});
+}
 
 //execution
 weeGrid();
+uiux();
+document.querySelector('body').classList.add('ready')
+
+//jQuery functions
+// TODO animatedScroll -> Refused to load the script 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js' because it violates the following Content Security Policy directive: "script-src 'self' https://www.googleapis.com".
+$(document).ready(function(){
+	var animatedScroll = function(){
+    $('a[href^=#]').click(function(){
+      var $target = $(this.hash);
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        if($('body').hasClass('scrolled')){
+        	$('html,body').delay(50).animate({scrollTop: targetOffset}, 250);
+        }else{
+        	$('html,body').delay(50).animate({scrollTop: targetOffset - 120 + 'px'}, 150);
+        }
+        return false;
+      }
+    });
+  }();
+});
